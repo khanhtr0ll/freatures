a66932045f142e96c63268eef986aed2d4875cae
khanhtr0ll <111417027+khanhtr0ll@users.noreply.github.com>
2023-03-29 23:14:36 +0700

    Delete author.js

--- test2/routes/author.js
+++
@@ -1,9 +0,0 @@
-const authorcontroller = require("../controllers/authorcontroller")
-
-const router = require("express").Router()
-router.post("/",authorcontroller.addauthor)
-router.get("/",authorcontroller.getallauthor)
-router.get("/:id",authorcontroller.getanauthor)
-route.put("/:id",authorcontroller.updatauthor)
-route.delete("/:id",authorcontroller.deleteauthor)
-module.exports = router
